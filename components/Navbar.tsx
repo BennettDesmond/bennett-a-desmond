f8e07e9edd907656588053166768f19540f501cf
Bennett Desmond <bennett.a.desmond@gmail.com>
2022-03-09 11:19:32 -0800

    Adding github pages ability

---
+++ components/Navbar.tsx
@@ -0,0 +1,16 @@
+import {
+    Text,
+} from '@chakra-ui/react'
+
+interface NavbarProps {
+}
+
+export default function Navbar(props: NavbarProps) {
+    return (
+        <>
+            <Text>
+                Navbar Section
+            </Text>
+        </>
+    )
+}
