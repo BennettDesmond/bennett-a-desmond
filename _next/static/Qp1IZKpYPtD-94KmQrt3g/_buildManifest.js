74ddb217e3b9e67b3f4c953fa6b58f525fce2384
BennettDesmond <bennett.a.desmond@gmail.com>
2022-03-09 19:34:15 +0000

    Deploying to gh-pages from @ BennettDesmond/bennett-a-desmond@bfcb07bf4a2e41fbc2d2bea0523f66189dd608fd 🚀

---
+++ _next/static/Qp1IZKpYPtD-94KmQrt3g/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/199-4974506ba96aa22e.js","static/chunks/pages/index-622c90d4e4dfd6c4.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
