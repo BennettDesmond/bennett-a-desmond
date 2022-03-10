fee3cdff777fb9ac34ef75d32420132774faddb4
Bennett Desmond <bennett.a.desmond@gmail.com>
2022-03-09 17:27:24 -0800

    Added basic footer file

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import { Heading } from "@chakra-ui/react";
+
+interface ExperienceProps {}
+
+export default function Experience(props: ExperienceProps) {
+  return (
+    <>
+      <Heading>Experience Section</Heading>
+    </>
+  );
+}
