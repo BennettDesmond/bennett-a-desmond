26f38e95c313a78b7a8778aed482b02d6c958858
Bennett Desmond <bennett.a.desmond@gmail.com>
2022-03-14 17:28:48 -0700

    Checking if this resolves deployment

--- components/navbar/NavLink.tsx
+++ components/navbar/NavLink.tsx
@@ -8,20 +8,18 @@ interface NavLinkProps {
 export default function NavLink(props: NavLinkProps) {
   const { linkText, link } = props;
   return (
-    <>
-      <Link
-        px={2}
-        py={1}
-        rounded={"md"}
-        _hover={{
-          textDecoration: "none",
-          bg: useColorModeValue("gray.200", "gray.700"),
-        }}
-        href={"#" + link}
-        fontWeight="semibold"
-      >
-        {linkText}
-      </Link>
-    </>
+    <Link
+      px={2}
+      py={1}
+      rounded={"md"}
+      _hover={{
+        textDecoration: "none",
+        bg: useColorModeValue("gray.200", "gray.700"),
+      }}
+      href={"#" + link}
+      fontWeight="semibold"
+    >
+      {linkText}
+    </Link>
   );
 }
